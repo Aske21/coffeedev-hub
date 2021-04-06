65b139b8dc4888210e5d4083b640b95de1476b3a
Aske21 <veledarevica@yahoo.com>
2021-04-06 21:29:34 +0200

    preparing to host to heroku

--- rest/dao/MessageDao.class.php
+++ rest/dao/MessageDao.class.php
@@ -1,4 +1,13 @@
 <?php
 
+require_once dirname(__FILE__)."../BaseDao.class.php";
+
+class MessageDao extends BaseDao{
+
+    public function __construct(){
+        parent::__construct("messages");
+    }
+
+}
 
 ?>
