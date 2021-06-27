53fb11e313576566ab611ed1027f03fd8708a9e9
Aske21 <veledarevica@yahoo.com>
2021-06-27 16:30:51 +0200

    tweaked db dump, added fk constraints, etc

---
+++ socialnetwork_dump.sql
@@ -0,0 +1,140 @@
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 8.0.23 : Database - socialnetwork
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`socialnetwork` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `socialnetwork`;
+
+/*Table structure for table `comments` */
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `comment` text NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `posted_at` datetime NOT NULL,
+  `post_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_comment_user` (`user_id`),
+  CONSTRAINT `fk_comment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `comments` */
+
+insert  into `comments`(`id`,`comment`,`user_id`,`posted_at`,`post_id`) values 
+(2,'this is a test comment',1,'2021-06-27 14:27:08',1);
+
+/*Table structure for table `followers` */
+
+DROP TABLE IF EXISTS `followers`;
+
+CREATE TABLE `followers` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  `follower_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_follower_user` (`user_id`),
+  CONSTRAINT `fk_follower_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `followers` */
+
+/*Table structure for table `login_tokens` */
+
+DROP TABLE IF EXISTS `login_tokens`;
+
+CREATE TABLE `login_tokens` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `token` char(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'UNIQUE',
+  `user_id` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `login_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `login_tokens` */
+
+/*Table structure for table `messages` */
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `body` text NOT NULL,
+  `sender` int unsigned NOT NULL,
+  `receiver` int unsigned NOT NULL,
+  `read` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `messages` */
+
+/*Table structure for table `post_likes` */
+
+DROP TABLE IF EXISTS `post_likes`;
+
+CREATE TABLE `post_likes` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` int unsigned NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_postlike_post` (`post_id`),
+  KEY `fk_postlike_user` (`user_id`),
+  CONSTRAINT `fk_postlike_post` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `fk_postlike_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `post_likes` */
+
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `body` varchar(160) NOT NULL DEFAULT '',
+  `posted_at` datetime NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `likes` int unsigned NOT NULL,
+  `postimg` varchar(255) DEFAULT NULL,
+  `topics` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `posts` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `password` varchar(60) DEFAULT NULL,
+  `email` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `surname` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`password`,`email`,`name`,`surname`) values 
+(1,'1234','aske@gmail.com','Asim','Veledarevic'),
+(2,'12345','aske@gmail.com','Asim','Veledarevic');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
